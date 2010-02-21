a017ac4dc97a9959f8d062397e9ce797b9108e23
rofetsch@gmx.de <rofetsch@gmx.de@e90f58b4-dd91-11de-9d50-21f6430a363f>
2010-02-21 11:09:50 +0000

    Now supporting new taggedVaue "valueObject". This tagValue will only recognized if assigned to a class. For each class with a taggedValue "valueObject = true", now an appropriate value object class will be generated. Since not all classes have to have that tag all relations are ignored in the generated value object class.

--- trunk/GrailsMDA/src/main/GrailsMdaMain.java
+++ trunk/GrailsMDA/src/main/GrailsMdaMain.java
@@ -8,23 +8,38 @@ public class GrailsMdaMain {
 
 	/**
 	 * @param args
+	 *            First argument is the path to the UML model file, second is
+	 *            path to the project in which all classes should be generated
+	 *            into, third argument is the path to the model processor
 	 */
 	public static void main(String[] args) {
-		// <!-- model source file -->
-		// <arg value="${modelpath}" />
-		// <!-- folder to drop into -->
-		// <arg value="${genpath}" />
-		// <!-- custom model processor-->
-		// <arg value="${modelprocessorpath}" />
-		String[] bla = new String[3];
-		bla[0] = args[0];
-		// set generating path to
-		bla[1] = args[1];
-		// set specific modelprocessor
-		// bla[2] = "file:/templates/domain/DomainModelProcessor.groovy";
-		URL test = Thread.currentThread().getContextClassLoader().getResource("templates/domain/DomainModelProcessor.groovy");
-		bla[2] = "jar:" + test.getPath();
+		/**
+		 * As defined in the example build.xml <!-- model source file --> <arg
+		 * value="${modelpath}" /> <!-- folder to drop into --> <arg
+		 * value="${genpath}" /> <!-- custom model processor--> <arg
+		 * value="${modelprocessorpath}" />
+		 */
 
-		GroovyMDA.main(bla);
+		// Generating Domain classes
+		String[] extendedArgs = new String[3];
+		extendedArgs[0] = args[0];
+		// set generating path to
+		extendedArgs[1] = args[1] + "/grails-app/domain";
+		// set specific modelprocessor
+		URL processorPath = Thread.currentThread().getContextClassLoader().getResource("templates/domain/DomainModelProcessor.groovy");
+		extendedArgs[2] = "jar:" + processorPath.getPath();
+
+		GroovyMDA.main(extendedArgs);
+
+		// Generating ValueObject classes
+		extendedArgs = new String[3];
+		extendedArgs[0] = args[0];
+		// set generating path to
+		extendedArgs[1] = args[1] + "/src/groovy";
+		// set specific modelprocessor
+		processorPath = Thread.currentThread().getContextClassLoader().getResource("templates/valueobject/ValueObjectProcessor.groovy");
+		extendedArgs[2] = "jar:" + processorPath.getPath();
+
+		GroovyMDA.main(extendedArgs);
 	}
 }
--- trunk/GrailsMDA/src/templates/valueobject/ValueObjectProcessor.groovy
+++ trunk/GrailsMDA/src/templates/valueobject/ValueObjectProcessor.groovy
@@ -1,21 +1,17 @@
-package templates.domain
+package templates.valueobject
+
 import groovy.text.SimpleTemplateEngine 
 import org.omg.uml.foundation.core.Attribute
 import org.omg.uml.foundation.core.DataType
 import org.omg.uml.foundation.core.UmlClass 
-import org.omg.uml.foundation.datatypes.AggregationKindEnum
 import org.omg.uml.modelmanagement.Model 
-import groovy.text.SimpleTemplateEngine
-import org.omg.uml.foundation.core.Attribute
-import org.omg.uml.foundation.core.DataType
-import org.omg.uml.foundation.core.UmlClass
-import org.omg.uml.foundation.datatypes.OrderingKindEnum
-import org.omg.uml.modelmanagement.Model
 
 
 
-class DomainModelProcessor  {
+class ValueObjectProcessor  {
 	
+	static final String VALUE_OBJECT_TAG = "valueObject"
+	static final String VALUE_OBJECT_PACKAGE_NAME = "vo"
 	
 	def getPackageName = { modelElement ->
 		def namespace = modelElement
@@ -31,6 +27,7 @@ class DomainModelProcessor  {
 	
 	def getFullyQualifiedName = { modelElement ->
 		def className = modelElement.name
+		//add 
 		def packageName = getPackageName(modelElement)
 		return packageName.length() > 0 ? "${packageName}.${className}" : className
 	}
@@ -243,59 +240,22 @@ class DomainModelProcessor  {
 			
 			// GET THE FULLY QUALIFIED NAME FOR THE CLASS
 			def fullyQualifiedName = getFullyQualifiedName(context.currentModelElement)
+			def className = context.currentModelElement.name
+			//add .vo. to packge definition
+			fullyQualifiedName = fullyQualifiedName.replace(".${className}", ".${VALUE_OBJECT_PACKAGE_NAME}.${className}VO")
+			
 			
 			// ONLY PROCESS NON JRE CLASSES (java.lang.String does not need to be generated)
-			if (!fullyQualifiedName.startsWith("java") && fullyQualifiedName.size() > 0) {
-				println("[Generating DomainClass] ${fullyQualifiedName}")
+			if (!fullyQualifiedName.startsWith("java") && fullyQualifiedName.size() > 0 && isValueObjectTaggedValueSet(context.currentModelElement)) {
+				println("[Generating ValueObject] ${fullyQualifiedName}")
 				// YOU CAN BIND CLOSURES TO THE CONTEXT TO MAKE THEM ACCESSIBLE TO YOU TEMPLATES   
 				context.isComposite= { end ->
 					return end.aggregation == AggregationKindEnum.AK_COMPOSITE
 					//.equals(AggregationKindEnum.COMPOSITE.typeName) 
 				}
 				
-				context.taggedValueMap = { taggedValueKey, taggedValueModel ->
-					def tags = [:]
-					taggedValueModel.taggedValue.each { taggedValue ->
-						def key = taggedValue.type.name
-						if(key==taggedValueKey){
-							def valueBuffer = new StringBuffer()
-							taggedValue.dataValue.each { value ->
-								if (valueBuffer.length() > 0) {
-									valueBuffer.append(",")
-								}
-								valueBuffer.append(value)
-							}
-							if(tags.containsKey("${key}")){
-								key="${key}"+(tags.size()+1)
-							}
-							tags.put("${key}", "${valueBuffer.toString()}")
-						}
-					}
-					return tags
-				}
-				
-				context.constraintValueMap = { constraintValueKey, constraintValueModel ->
-					def tags = [:]
-					constraintValueModel.constraint.each { constraint ->
-						def key = constraint.name
-						if(key==constraintValueKey){
-							def valueBuffer = new StringBuffer()
-							constraint.body.each { value ->
-								if (valueBuffer.length() > 0) {
-									valueBuffer.append(",")
-								}
-								valueBuffer.append(value)
-							}
-							if(tags.containsKey("${key}")){
-								key="${key}"+(tags.size()+1)
-							}
-							tags.put("${key}", "${valueBuffer.toString()}")
-						}
-					}
-					return tags
-				}
 				// SET THE TEMPLATE TO USE
-				def templateName = "templates/domain/DomainModel.gtl"
+				def templateName = "templates/valueobject/ValueObject.gtl"
 				
 				// SET THE OUTPUT FILE NAME FOR THE FULLY QUALIFIED NAME
 				def outputName = "${fullyQualifiedName.replace('.','/')}.groovy"
@@ -305,6 +265,19 @@ class DomainModelProcessor  {
 				
 			}
 		}
+	}
+	
+	boolean isValueObjectTaggedValueSet(def model){
+		boolean isValueObjectTaggedValueSet = false
+		model.taggedValue.each { taggedValue ->
+			def key = taggedValue.type.name
+			if(key==VALUE_OBJECT_TAG){
+				taggedValue.dataValue.each{ value ->
+					isValueObjectTaggedValueSet = "${value}" as Boolean 
+				}
+			}
+		}
+		return isValueObjectTaggedValueSet
 	}
 	
 }
